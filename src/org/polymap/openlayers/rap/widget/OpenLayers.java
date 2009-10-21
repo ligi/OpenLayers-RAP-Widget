b9c8f994a784dc5755b0b56a19b74e976c917a8f
ligi <ligi@42dd07d8-1ee8-11de-aac7-e55ba83f00cf>
2009-10-21 15:43:55 +0000

    seperate map from widget
    git-svn-id: https://polymap.org/repos/polymap3/trunk/org.polymap.rap.widget.openlayers/widget@1407 42dd07d8-1ee8-11de-aac7-e55ba83f00cf

--- src/org/polymap/openlayers/rap/widget/OpenLayers.java
+++ src/org/polymap/openlayers/rap/widget/OpenLayers.java
@@ -29,6 +29,7 @@ import org.eclipse.swt.widgets.Layout;
 
 import org.polymap.openlayers.rap.widget.base.OpenLayersEvents;
 import org.polymap.openlayers.rap.widget.base.OpenLayersWidgetProvider;
+import org.polymap.openlayers.rap.widget.base_types.OpenLayersMap;
 import org.polymap.openlayers.rap.widget.controls.Control;
 import org.polymap.openlayers.rap.widget.layers.Layer;
 
@@ -44,7 +45,8 @@ public class OpenLayers extends Composite {
 
 	public boolean init_done;
 	public Vector<Object[]> cmd_stack;
-	public OpenLayersEvents events;
+
+	public OpenLayersMap map;
 
 	// default external location
 	public String js_location = "http://www.openlayers.org/api/OpenLayers.js";
@@ -61,14 +63,15 @@ public class OpenLayers extends Composite {
 		js_location = lib_location;
 	}
 
+	public OpenLayersMap getMap() {
+		return map;
+	}
+
 	public void prepare() {
 		cmd_stack = new Vector<Object[]>();
 		OpenLayersWidgetProvider.getInstance().setWidget(this);
-		events = new OpenLayersEvents(this);
+		map = new OpenLayersMap();
 	}
-
-	//
-	// String js_location="/lib/OpenLayers/OpenLayers.js";
 
 	public String getJSLocation() {
 		return js_location;
@@ -97,38 +100,6 @@ public class OpenLayers extends Composite {
 		Object[] res = cmd_stack.elementAt(0);
 		cmd_stack.removeElementAt(0);
 		return res;
-	}
-
-	public void addLayer(Layer layer2add) {
-		addCommand("addLayer", layer2add.getObjRef());
-	}
-
-	public void addControl(Control control2add) {
-		addCommand("addControl", control2add.getObjRef());
-	}
-
-	public void setProxy(String proxy){
-		addCommand("map_eval", "OpenLayers.ProxyHost='"+proxy+"';");
-	}
-	
-	public void removeControl(Control control2rm) {
-		addCommand("map_eval", "this._map.removeControl("
-				+ control2rm.getJSObjRef() + ");");
-	}
-
-	public void zoomTo(int zoom) {
-		Object[] param_arr = { zoom };
-		addCommand("zoomTo", param_arr);
-	}
-
-	public void setCenter(double center_lon, double center_lat) {
-		Object[] param_arr = { center_lon, center_lat };
-		addCommand("setCenter", param_arr);
-	}
-
-	public void setBaseLayer(Layer layer) {
-		addCommand("map_eval", "this._map.setBaseLayer(" + layer.getJSObjRef()
-				+ ");");
 	}
 
 }
---
+++ src/org/polymap/openlayers/rap/widget/base_types/OpenLayersMap.java
@@ -0,0 +1,80 @@
+/*
+ * polymap.org
+ * Copyright 2009, Polymap GmbH, and individual contributors as indicated
+ * by the @authors tag.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+package org.polymap.openlayers.rap.widget.base_types;
+
+import org.polymap.openlayers.rap.widget.base.OpenLayersObject;
+import org.polymap.openlayers.rap.widget.controls.Control;
+import org.polymap.openlayers.rap.widget.layers.Layer;
+
+/**
+ * 
+ * @author Marcus -LiGi- B&uuml;schleb < mail: ligi (at) polymap (dot) de >
+ * 
+ */
+
+public class OpenLayersMap extends OpenLayersObject {
+
+	public OpenLayersMap() {
+		super.create_out_of_hash("this._map");
+	}
+	
+	public void addLayer(Layer layer2add) {
+		super.addObjModCode("addLayer",layer2add);
+		//addCommand("addLayer", layer2add.getObjRef());
+	}
+
+	public void addControl(Control control2add) {
+		super.addObjModCode("addControl",control2add);
+		//addCommand("addControl", control2add.getObjRef());
+	}
+
+	public void setProxy(String proxy){
+		super.addObjModCode("OpenLayers.ProxyHost='"+proxy+"';");
+		//addCommand("map_eval", "OpenLayers.ProxyHost='"+proxy+"';");
+	}
+	
+	public void removeControl(Control control2rm) {
+		super.addObjModCode("removeControl",control2rm);
+		//addCommand("map_eval", "this._map.removeControl("
+	//			+ control2rm.getJSObjRef() + ");");
+	}
+
+	public void zoomTo(int zoom) {
+		super.addObjModCode("zoomTo",zoom);
+		//Object[] param_arr = { zoom };
+		//addCommand("zoomTo", param_arr);
+	}
+
+	public void setCenter(double center_lon, double center_lat) {
+		super.addObjModCode("setCenter", new LonLat(center_lon,center_lat));
+		//Object[] param_arr = { center_lon, center_lat };
+		//addCommand("setCenter", param_arr);
+	}
+
+	public void setBaseLayer(Layer layer) {
+		super.addObjModCode("setBaseLayer",layer);
+		//addCommand("map_eval", "this._map.setBaseLayer(" + layer.getJSObjRef()
+		//		+ ");");
+	}
+
+}
