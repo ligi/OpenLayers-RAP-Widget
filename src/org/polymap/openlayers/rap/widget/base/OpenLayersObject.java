5207e772187035318d9f8e2d9cdfca0e20b7c685
ligi <ligi@42dd07d8-1ee8-11de-aac7-e55ba83f00cf>
2009-10-28 11:56:20 +0000

    adding methods abstract modifying the attributes of the js object
    git-svn-id: https://polymap.org/repos/polymap3/trunk/org.polymap.rap.widget.openlayers/widget@1442 42dd07d8-1ee8-11de-aac7-e55ba83f00cf

--- src/org/polymap/openlayers/rap/widget/base/OpenLayersObject.java
+++ src/org/polymap/openlayers/rap/widget/base/OpenLayersObject.java
@@ -33,94 +33,128 @@ import org.polymap.openlayers.rap.widget.OpenLayersWidget;
 
 public class OpenLayersObject {
 
-	private OpenLayersWidget widget = null;
-	private String obj_ref = null;
-	private String obj_mod_code = "";
-	public OpenLayersEvents events;
+    private OpenLayersWidget widget       = null;
 
-	public OpenLayersObject() {
-		events = new OpenLayersEvents(this);
-	}
+    private String           obj_ref      = null;
 
-	public void addObjModCode(String code) {
-		obj_mod_code += code;
-		changes2widget();
-	}
+    private String           obj_mod_code = "";
 
-	public void addObjModCode(String function, OpenLayersObject obj) {
-		addObjModCode(getJSObjRef() + "." + function + "(" + obj.getJSObjRef()
-				+ ");");
-	}
+    public OpenLayersEvents  events;
 
-	public void addObjModCode(String function, int val) {
-		addObjModCode(getJSObjRef() + "." + function + "(" + val + ");");
-	}
 
-	public void addObjModCode(String function, boolean val) {
-		addObjModCode(getJSObjRef() + "." + function + "(" + val + ");");
-	}
+    public OpenLayersObject() {
+        events = new OpenLayersEvents( this );
+    }
 
-	public void addObjModCode(String function, double val) {
-		addObjModCode(getJSObjRef() + "." + function + "(" + val + ");");
-	}
 
-	public OpenLayersWidget getWidget() {
-		if (widget == null) {
-			OpenLayersWidgetProvider wp = OpenLayersWidgetProvider
-					.getInstance();
-			this.widget = wp.getWidget();
-		}
-		return widget;
-	}
+    public void addObjModCode( String code ) {
+        obj_mod_code += code;
+        changes2widget();
+    }
 
-	public void create(String js_create_code) {
-		OpenLayersWidgetProvider wp = OpenLayersWidgetProvider.getInstance();
-		this.setObjRef(wp.generateObjectReference("o",this));
-		getWidget().addCommand("eval", getJSObjRef() + "=" + js_create_code);
-	}
 
-	public void changes2widget() {
-		if (getWidget()!= null) {
-			if (obj_mod_code != "")
-				widget.addCommand("eval", "obj=" + getJSObjRef() + "; "
-						+ obj_mod_code);
+    public void addObjModCode( String function, OpenLayersObject obj ) {
+        addObjModCode( getJSObjRef() + "." + function + "(" + obj.getJSObjRef() + ");" );
+    }
 
-			obj_mod_code = "";
-		}
-	}
 
-	public void setObjRef(String obj_ref) {
-		this.obj_ref = obj_ref;
-	}
+    public void addObjModCode( String function, int val ) {
+        addObjModCode( getJSObjRef() + "." + function + "(" + val + ");" );
+    }
 
-	public String getObjRef() {
-		return obj_ref;
-	}
 
-	public String getJSObjRef() {
-		return "objs['" + obj_ref + "']";
-	}
+    public void addObjModCode( String function, boolean val ) {
+        addObjModCode( getJSObjRef() + "." + function + "(" + val + ");" );
+    }
 
-	public String getJSObj(OpenLayersObject object) {
-		if (object == null)
-			return "null";
-		else
-			return object.getJSObjRef();
-	}
 
-	public String getJSObj(OpenLayersObject[] oa) {
-		if (oa == null)
-			return "[null]";
-		else {
-			String res = "[";
-			for (OpenLayersObject obj : oa) {
-				if (!res.equals("["))
-					res += ",";
-				res += getJSObj(obj);
-			}
-			return res + "]";
-		}
+    public void addObjModCode( String function, double val ) {
+        addObjModCode( getJSObjRef() + "." + function + "(" + val + ");" );
+    }
 
-	}
+
+    public void setObjAttr( String attr, OpenLayersObject obj ) {
+        addObjModCode( getJSObjRef() + "." + attr + "=" + obj.getJSObjRef() + ";" );
+    }
+
+
+    public void setObjAttr( String attr, int val ) {
+        addObjModCode( getJSObjRef() + "." + attr + "=" + val + ";" );
+    }
+
+
+    public void setObjAttr( String attr, boolean val ) {
+        addObjModCode( getJSObjRef() + "." + attr + "=" + val + ");" );
+    }
+
+
+    public void setObjAttr( String attr, double val ) {
+        addObjModCode( getJSObjRef() + "." + attr + "=" + val + ";" );
+    }
+
+
+    public OpenLayersWidget getWidget() {
+        if (widget == null) {
+            OpenLayersWidgetProvider wp = OpenLayersWidgetProvider.getInstance();
+            this.widget = wp.getWidget();
+        }
+        return widget;
+    }
+
+
+    public void create( String js_create_code ) {
+        OpenLayersWidgetProvider wp = OpenLayersWidgetProvider.getInstance();
+        this.setObjRef( wp.generateObjectReference( "o", this ) );
+        getWidget().addCommand( "eval", getJSObjRef() + "=" + js_create_code );
+    }
+
+
+    public void changes2widget() {
+        if (getWidget() != null) {
+            if (obj_mod_code != "")
+                widget.addCommand( "eval", "obj=" + getJSObjRef() + "; " + obj_mod_code );
+
+            obj_mod_code = "";
+        }
+    }
+
+
+    public void setObjRef( String obj_ref ) {
+        this.obj_ref = obj_ref;
+    }
+
+
+    public String getObjRef() {
+        return obj_ref;
+    }
+
+
+    public String getJSObjRef() {
+        return "objs['" + obj_ref + "']";
+    }
+
+
+    public String getJSObj( OpenLayersObject object ) {
+        if (object == null)
+            return "null";
+        else
+            return object.getJSObjRef();
+    }
+
+
+    public String getJSObj( OpenLayersObject[] oa ) {
+        if (oa == null)
+            return "[null]";
+        else {
+            String res = "[";
+            for (OpenLayersObject obj : oa) {
+                if (!res.equals( "[" ))
+                    res += ",";
+                res += getJSObj( obj );
+            }
+            return res + "]";
+        }
+
+    }
 
 }
