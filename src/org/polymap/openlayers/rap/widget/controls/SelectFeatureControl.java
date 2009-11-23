1dfa8d2123a2e005b49769e09706daf5cb9f3284
ligi <ligi@42dd07d8-1ee8-11de-aac7-e55ba83f00cf>
2009-11-23 21:18:22 +0000

    merging falkos changes and mine
    git-svn-id: https://polymap.org/repos/polymap3/trunk/org.polymap.rap.widget.openlayers/widget@1542 42dd07d8-1ee8-11de-aac7-e55ba83f00cf

--- src/org/polymap/openlayers/rap/widget/controls/SelectFeatureControl.java
+++ src/org/polymap/openlayers/rap/widget/controls/SelectFeatureControl.java
@@ -43,38 +43,88 @@ public class SelectFeatureControl extends Control {
     
     public boolean started_with_hover_enabled = false;
 	
+    public boolean started_with_hover_enabled = false;
+    public boolean started_with_box_enabled = false;
+
+
 	public SelectFeatureControl(Layer layer) {
 		super.create("new OpenLayers.Control.SelectFeature("
 				+ layer.getJSObjRef() + ");");
 	}
-	
-	public SelectFeatureControl(Layer layer,boolean hover) {
-		started_with_hover_enabled=hover;
+
+	public SelectFeatureControl(Layer layer, boolean hover) {
+		started_with_hover_enabled = hover;
 		super.create("new OpenLayers.Control.SelectFeature("
-				+ layer.getJSObjRef() + ", {    multiple: false, hover: " + hover + " } );");
+				+ layer.getJSObjRef() + ", {    multiple: false, hover: "
+				+ hover + " } );");
+	}
+
+	public SelectFeatureControl(Layer layer, boolean hover, boolean box) {
+		started_with_hover_enabled = hover;
+		started_with_box_enabled = box;
+		super.create("new OpenLayers.Control.SelectFeature("
+				+ layer.getJSObjRef() + ", {    multiple: false, hover: "
+				+ hover + ", box: " + box + " } );");
 	}
 
 	public SelectFeatureControl(Layer[] layers) {
-		super.create("new OpenLayers.Control.SelectFeature("
-				+ getJSObj(layers) + ");");
+		super.create("new OpenLayers.Control.SelectFeature(" + getJSObj(layers)
+				+ ");");
 	}
-	
+
 	/*
-	 * 	{Boolean} Select on mouse over and deselect on mouse out. 
+	 * {Boolean} Select on mouse over and deselect on mouse out.
 	 */
-	
+
 	public void setHover(boolean hover) {
 		assert started_with_hover_enabled : "if you want to toggle hoover you have to start with hover=true in the constructor";
 		super.setObjAttr("hover", hover);
 	}
-	
+
+	/*
+	 * {Boolean} Allow feature selection by drawing a box.
+	 */
+
+	public void setBox(boolean box) {
+		assert started_with_hover_enabled : "if you want to toggle box selection you have to start with box=true in the constructor";
+		super.setObjAttr("box", box);
+	}
+
+	/*
+	 * {String} An event modifier ("altKey" or "shiftKey") that temporarily sets
+	 * the multiple property to true. Default is null.
+	 */
+
+	public void setMultipleKey(String key) {
+		super.setObjAttr("multipleKey", key);
+	}
+
+	/*
+	 * {String} An event modifier ("altKey" or "shiftKey") that temporarily sets
+	 * the toggle property to true. Default is null.
+	 */
+
+	public void setToggleKey(String key) {
+		super.setObjAttr("toggleKey", key);
+	}
+
+	/*
+	 * {Boolean} Allow selection of multiple geometries. Default is false.
+	 */
+	public void setMultiple(boolean multiple) {
+		super.setObjAttr("multiple", multiple);
+	}
+
 	public void setRenderIntent(String intent) {
 		super.setObjAttr("renderIntent", intent);
 	}
+
 	/*
-	 * {Boolean} If true do not actually select features (i.e. place them in the layers selected features array), just highlight them.  This property has no effect if hover is false.  Defaults to false.
+	 * {Boolean} If true do not actually select features (i.e. place them in the
+	 * layers selected features array), just highlight them. This property has
+	 * no effect if hover is false. Defaults to false.
 	 */
 	public void setHighlightOnly(Boolean highlight_only) {
-		super.setObjAttr("highlightOnly",highlight_only);
+		super.setObjAttr("highlightOnly", highlight_only);
 	}
 }
